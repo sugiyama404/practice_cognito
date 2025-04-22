95a43d527cf7c1b557c7d5fac239332289fa2289
sugiyama404 <mmingriha@gmail.com>
2025-04-22 20:46:05 +0900

    Fix Lambda function handler reference in Terraform configuration

--- infra/modules/lambda/aws_lambda_function.tf
+++ infra/modules/lambda/aws_lambda_function.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_function" "protected_endpoint" {
   function_name    = "protected_endpoint"
   filename         = data.archive_file.func1.output_path
-  handler          = "main.handler"
+  handler          = "main.lambda_handler"
   source_code_hash = data.archive_file.func1.output_base64sha256
   role             = var.lambda_authorizer_iam_role_arn
   runtime          = "python3.12"
